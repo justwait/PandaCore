bd68c52d6dd7d0f3861e2c4eaa1d7d4d765eda6e
Panda-Project <kentattitude@live.fr>
2012-05-21 22:11:11 +0300

    Update src/game/Scripts/cataclysm/instance/BlackrockCaverns/boss_ascendant_lord_obsidius.cpp

--- src/game/Scripts/cataclysm/instance/BlackrockCaverns/boss_ascendant_lord_obsidius.cpp
+++ src/game/Scripts/cataclysm/instance/BlackrockCaverns/boss_ascendant_lord_obsidius.cpp
@@ -15,7 +15,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "ScriptPCH.h"
+#include "pchdef.h"
 #include "instance_blackrock_caverns.h"
 
 enum Spells
